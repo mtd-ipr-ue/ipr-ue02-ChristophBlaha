29d08bdb62e40cb43ca62796de7722d2918cf7a3
Christoph Blaha <christophblaha646@gmail.com>
2024-10-28 15:13:22 +0100

    Ue 1 fertig

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,24 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Out.print("Zahl 1: ");
+        int num1 = In.readInt();
+        Out.print("Zahl 2: ");
+        int num2 = In.readInt();
+        Out.print("Zahl 3: ");
+        int num3 = In.readInt();
+
+        int max = num1;
+
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+        }
+
+        Out.print("Die höchste Zahl ist " + max);
     }
 }
