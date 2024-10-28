502d59ded426bc3be59b14c2a57f5ac13a48994f
Christoph Blaha <christophblaha646@gmail.com>
2024-10-28 15:42:21 +0100

    Übung 3 fertig

--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,19 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_3 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        Out.print("x-Koordinate: ");
+        double x = In.readDouble();
+        Out.print("y-Koordinate: ");
+        double y = In.readDouble();
+
+        double z = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        Out.println(z);
+        if (x > 0 && y > 0 && z > 4) {
+            Out.println("Der Punkt liegt außerhalb des Kreises");
+        } else {
+            Out.println("Der Punkt liegt innerhalb des Kreises");
+        }
     }
 }
